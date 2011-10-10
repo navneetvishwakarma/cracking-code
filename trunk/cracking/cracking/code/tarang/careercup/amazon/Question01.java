831ad60e951c2f51f03fb997b0a9b1b14d9ff3c5
nphardcode@gmail.com <nphardcode@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2011-10-10 22:51:07 +0000

    some refactoring

---
+++ trunk/cracking/cracking/code/tarang/careercup/amazon/Question01.java
@@ -0,0 +1,35 @@
+package cracking.code.tarang.careercup.amazon;
+
+import cracking.code.tarang.careercup.TreeNode;
+
+
+/**
+ * Find if a binary tree is bst
+ *
+ * @author tdesai
+ */
+public class Question01 {
+
+    public boolean check(TreeNode node) {
+        if (node == null) {
+            return true;
+        } else {
+            int value = node.getValue();
+            if (node.getLeft() != null && value < node.getLeft().getValue()) {
+                return false;
+            }
+            if (node.getRight() != null && value > node.getRight().getValue()) {
+                return false;
+            }
+            return check(node.getLeft()) && check(node.getRight());
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(3, new TreeNode(0), new TreeNode(5));
+        TreeNode right = new TreeNode(9, new TreeNode(8), new TreeNode(11));
+        TreeNode root = new TreeNode(7, left, right);
+        System.out.println(new Question01().check(root));
+    }
+}
+
