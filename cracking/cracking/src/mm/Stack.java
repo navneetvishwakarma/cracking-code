9e073e61511353bf0106fafa7b6f63f7d7ee5c24
aatmaram.dubey <aatmaram.dubey@gmail.com>
2011-10-11 00:17:39 +0000

    commit

--- cracking/cracking/src/mm/Stack.java
+++ cracking/cracking/src/mm/Stack.java
@@ -1,6 +1,9 @@
 package cracking.src.mm;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.EmptyStackException;
+import java.util.List;
 
 
 /**
@@ -9,7 +12,7 @@ import java.util.EmptyStackException;
  * @author mmathuria
  */
 public class Stack {
-    
+
     private int[] store;
     private int topPtr;
 
@@ -18,37 +21,52 @@ public class Stack {
         store = new int[size];
         topPtr = -1;
     }
-    
-    public void push(int i){
-        if(isFull())    return;
+
+    public void push(int i) {
+        if (isFull()) return;
         store[++topPtr] = i;
     }
-    
-    
-    public int pop() throws EmptyStackException{
-        if(isEmpty())   throw new EmptyStackException();
+
+
+    public int pop() throws EmptyStackException {
+        if (isEmpty()) throw new EmptyStackException();
         return store[topPtr--];
     }
 
-    public boolean isFull(){
-        return (topPtr == store.length-1);    
+    public boolean isFull() {
+        return (topPtr == store.length - 1);
     }
-    
-    
-    public boolean isEmpty(){
+
+
+    public boolean isEmpty() {
         return (topPtr == -1);
     }
 
-    public int count(){
+    public int count() {
         return (topPtr + 1);
     }
-    
+
+    public int peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return store[topPtr];
+    }
+
     @Override
     public String toString() {
         StringBuffer sbuf = new StringBuffer();
-        for(int i=store.length-1;i>=0;i--){
+        for (int i = store.length - 1; i >= 0; i--) {
             sbuf.append(store[i]).append("\n");
         }
         return sbuf.toString();
+    }
+
+
+    public String toString2() {
+        StringBuffer sbuf = new StringBuffer();
+        sbuf.append("[");
+        for (int i = store.length - 1; i >= 0; i--) {
+            sbuf.append(store[i]).append(" ,");
+        }
+        return sbuf.append("]").toString();
     }
 }
---
+++ cracking/cracking/src/mm/chap3/Question3dot6.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2008 Bridge Entertainment, Inc. (dba E-Poll Market Research)
+ * All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information
+ * of Bridge Entertainment, Inc. ("Confidential Information").
+ */
+
+
+package cracking.src.mm.chap3;
+
+import cracking.src.mm.Stack;
+
+import java.util.EmptyStackException;
+
+
+/**
+ * Write a program to sort a stack in ascending order You should not make any assump- tions about how the stack is 
+ * implemented The following are the only functions that should be used to write this program: push | pop | peek | isEmpty
+ *
+ * @author mmathuria
+ */
+public class Question3dot6 {
+    
+    public Stack sortMyWay(Stack a){
+        Stack b = new Stack(a.count());
+        
+        for(int i=0; i<a.count(); i++){
+            int max = a.pop();
+            
+            
+            while(!a.isEmpty()){
+                int item = a.pop();
+                if(max < item){
+                    b.push(max);
+                    max = item;
+                }else{
+                    b.push(item);
+                }
+            }
+            
+            a.push(max);
+            while(!b.isEmpty()){
+                a.push(b.pop());
+            }
+            //System.out.println(a.toString2());
+        }
+
+        return a;
+    }
+}
