00f26dfc461cd306875b90ee5413c092838d3961
nphardcode <nphardcode@gmail.com>
2011-10-12 00:04:49 +0000

    amazon question8 solution

---
+++ cracking/cracking/src/tarang/careercup/amazon/Question08.java
@@ -0,0 +1,26 @@
+package tarang.careercup.amazon;
+
+
+/**
+ * If there is a n-digit, print the digits one by one without using temporary storage and how will you do it efficiently?
+ * Example: if the number is 1054 print: 1 0 5 4
+ *
+ * @author tdesai
+ */
+public class Question08 {
+
+    public static void print(int number) {
+        if(number == 0) {
+            return;
+        }
+        int digit = number % 10;
+        number /= 10;
+        print(number);
+        System.out.print(digit + " ");
+    }
+
+    public static void main(String[] args) {
+        System.out.print(1054 + " -> ");
+        print(1054);
+    }
+}
