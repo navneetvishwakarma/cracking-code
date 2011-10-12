c836dc74014a90853a3a7e8ea98c05cedb7e2ec3
nphardcode <nphardcode@gmail.com>
2011-10-12 21:02:02 +0000

    chapter 4 question 2.

---
+++ cracking/cracking/src/tarang/datastructures/GNode.java
@@ -0,0 +1,78 @@
+package tarang.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Graph node object
+ *
+ * @author tdesai
+ */
+public class GNode {
+
+    private int value;
+    private boolean discovered;
+    private List<GNode> adj;
+
+    public GNode(int value) {
+        this.value = value;
+        this.adj = new ArrayList<GNode>();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public List<GNode> getAdj() {
+        return adj;
+    }
+
+    public void setAdj(List<GNode> adj) {
+        this.adj = adj;
+    }
+
+    public boolean isDiscovered() {
+        return discovered;
+    }
+
+    public void setDiscovered(boolean discovered) {
+        this.discovered = discovered;
+    }
+
+    public void connnect(GNode node) {
+        this.adj.add(node);
+    }
+
+    @Override
+    public String toString() {
+        return value + "";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        GNode gNode = (GNode) o;
+
+        if (value != gNode.value) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+}
---
+++ cracking/cracking/src/tarang/datastructures/Graph.java
@@ -0,0 +1,47 @@
+package tarang.datastructures;
+
+import java.util.*;
+
+
+/**
+ * My very own graph
+ *
+ * @author tdesai
+ */
+public class Graph {
+
+    private Map<Integer, GNode> nodes;
+
+    public Graph(int[][] adj_matrix) {
+        if(adj_matrix == null || adj_matrix.length == 0) {
+            throw new IllegalArgumentException("adj matrix is null or empty");
+        }
+        nodes = new HashMap<Integer, GNode>();
+        // create a graph with the an adjacency list
+        GNode node1;
+        GNode node2;
+        for(int i = 0; i < adj_matrix.length; i++) {
+            int key1 = i+1;
+            node1 = nodes.get(key1);
+            if(node1 == null) {
+                node1 = new GNode(key1);
+                nodes.put(key1, node1);
+            }
+            for(int j = 0; j < adj_matrix[i].length; j++) {
+                int key2 = j+1;
+                node2 = nodes.get(key2);
+                if (node2 == null) {
+                    node2 = new GNode(key2);
+                    nodes.put(key2, node2);
+                }
+                if(adj_matrix[i][j] == 1) {
+                    node1.connnect(node2);
+                }
+            }
+        }
+    }
+
+    public Map<Integer, GNode> getNodes() {
+        return nodes;
+    }
+}
