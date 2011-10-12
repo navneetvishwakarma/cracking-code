93b1e369868ac98506e045cb2bdbf68e054a5c5b
nphardcode <nphardcode@gmail.com>
2011-10-12 21:03:45 +0000

    chapter 4 question 3.

---
+++ cracking/cracking/src/tarang/chap4/Question3.java
@@ -0,0 +1,12 @@
+package tarang.chap4;
+
+
+/**
+ * Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height
+ *
+ * @author tdesai
+ */
+public class Question3 {
+
+    // same as question 3 of careercup amazon question bank. Implemented there.
+}
