9f3ede8b4d5b62df4f662b3b8d8d28b75ab0be7e
aatmaram.dubey <aatmaram.dubey@gmail.com>
2011-10-13 00:16:34 +0000

    soln 3.2

---
+++ cracking/cracking/src/mm/chap3/Question3dot2.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2008 Bridge Entertainment, Inc. (dba E-Poll Market Research)
+ * All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information
+ * of Bridge Entertainment, Inc. ("Confidential Information").
+ */
+
+
+package mm.chap3;
+
+import mm.Stack;
+
+import java.util.EmptyStackException;
+
+
+/**
+ * How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum 
+ * element? Push, pop and min should all operate in O(1) time
+ *
+ * @author mmathuria
+ */
+public class Question3dot2 {
+    private Stack stackA;
+    private Stack stackMin;
+
+    public Question3dot2() {
+        stackA = new Stack(10);
+        stackMin = new Stack(10);
+    }
+    
+    public void push(int value){
+        if(stackA.isFull()) throw new IllegalStateException("Stack overflow");
+        
+        stackA.push(value);
+        
+        if(stackMin.isEmpty() || stackMin.peek() > value){
+            stackMin.push(value);
+        }
+    }
+    
+    public int pop(){
+        if(stackA.isEmpty()) throw new EmptyStackException();
+        
+        int item = stackA.pop();
+        if(stackMin.peek() == item){
+            stackMin.pop();
+        }
+        return item;
+    }
+    
+    public int min(){
+        if(stackA.isEmpty()) throw new EmptyStackException();
+        return stackMin.peek();
+    }
+    
+}
---
+++ cracking/cracking/test/mm/chap3/Question3dot2Test.java
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2008 Bridge Entertainment, Inc. (dba E-Poll Market Research)
+ * All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information
+ * of Bridge Entertainment, Inc. ("Confidential Information").
+ */
+
+
+package mm.chap3;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Put class JavaDoc here
+ *
+ * @author mmathuria
+ */
+public class Question3dot2Test extends TestCase {
+    
+    public void test1(){
+        Question3dot2 stack = new Question3dot2();
+        int min;
+
+
+        try {
+            stack.pop();
+            fail("Stack did not underflow");
+        } catch (Exception e) {
+            
+        }
+        
+        
+        stack.push(18);
+        stack.push(12);
+        stack.push(8);
+        stack.push(21);
+        
+        min = stack.min();
+        assertEquals("incorrect min returned",8,min);
+        
+        
+        stack.pop();
+        stack.pop();
+        min = stack.min();
+        assertEquals("incorrect min returned",12,min);
+        
+        stack.push(16);
+        stack.push(12);
+        stack.push(4);
+        stack.push(8);
+        
+        min = stack.min();
+        assertEquals("incorrect min returned", 4, min);
+    }
+}
