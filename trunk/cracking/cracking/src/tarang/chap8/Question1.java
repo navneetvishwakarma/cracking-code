7ccc3d202538a1a5ca3eb1a0cd3204c993f7b117
nphardcode@gmail.com <nphardcode@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2011-10-14 19:10:12 +0000

    chapter 8 question 1 iterative solution

--- trunk/cracking/cracking/src/tarang/chap8/Question1.java
+++ trunk/cracking/cracking/src/tarang/chap8/Question1.java
@@ -8,11 +8,30 @@ package tarang.chap8;
  */
 public class Question1 {
 
+
+    // recursive
     public static int fib(int i) {
         return i < 2 ? i : fib(i - 1) + fib(i - 2);
     }
 
+    // iterative
+    public static int fib2(int i) {
+        if(i < 2) {
+            return i;
+        }
+        int prev_1 = 1;
+        int prev_2 = 0;
+        int sum = 0;  // for n = 2
+        for(int k = 2; k <= i; k++) {
+            sum = prev_1 + prev_2;
+            prev_2 = prev_1;
+            prev_1 = sum;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(fib(10));
+        System.out.println(fib2(10));
     }
 }
