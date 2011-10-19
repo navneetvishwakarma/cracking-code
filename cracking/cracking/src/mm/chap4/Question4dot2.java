c9b46aec893746001258c3ca60e2d31dd4272c86
aatmaram.dubey <aatmaram.dubey@gmail.com>
2011-10-19 19:52:16 +0000

    question 4.2

---
+++ cracking/cracking/src/mm/chap4/Question4dot2.java
@@ -0,0 +1,50 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2008 Bridge Entertainment, Inc. (dba E-Poll Market Research)
+ * All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information
+ * of Bridge Entertainment, Inc. ("Confidential Information").
+ */
+
+
+package mm.chap4;
+
+import mm.Graph;
+import mm.Node;
+import mm.Vertex;
+
+
+/**
+ * Given a directed graph, design an algorithm to find out whether there is a route be- tween two nodes
+ *
+ * @author mmathuria
+ */
+public class Question4dot2 {
+    
+    public boolean hasPath(Graph g, Vertex start, Vertex end){
+        
+        start.setVisited(true);
+        
+        Vertex[] neighbors = g.getNeighbors(start);
+        if(isNeighbor(neighbors, end)){
+            return true;
+        }
+        
+        for(Vertex neighbor : neighbors){
+            if((!neighbor.isVisited()) && hasPath(g, neighbor, end)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNeighbor(Vertex[] neighbors, Vertex end) {
+        for(Vertex v : neighbors){
+            if(v.getData() == end.getData()) return true;
+        }
+        return false;
+    }
+
+}
