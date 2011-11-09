ca2d6f9077f506b9170831a2df573264adbb29d4
nphardcode <nphardcode@gmail.com>
2011-11-09 23:13:14 +0000

    incomplete for these two questions. Will take a shot at them later.

---
+++ cracking/cracking/src/tarang/chap8/Question2.java
@@ -0,0 +1,14 @@
+package tarang.chap8;
+
+
+/**
+ * Imagine a robot sitting on the upper left hand corner of an NxN grid The robot can only move in two directions: right
+ * and down How many possible paths are there for the robot?
+ * FOLLOW UP
+ * Imagine certain squares are “off limits”, such that the robot can not step on them Design an algorithm to get all
+ * possible paths for the robot
+ * todo: incomplete
+ * @author tdesai
+ */
+public class Question2 {
+}
