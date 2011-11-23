66cff1a0fa81afbe60a553e27f88043e00acbb02
aatmaram.dubey@gmail.com <aatmaram.dubey@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2011-11-23 02:42:28 +0000

    Find the maximum sum path in a binary tree

--- trunk/cracking/cracking/src/mm/amazon/Q07.java
+++ trunk/cracking/cracking/src/mm/amazon/Q07.java
@@ -1,5 +1,11 @@
 package mm.amazon;
 
+import mm.BinaryTree;
+import mm.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Find the maximum sum path in a binary tree
@@ -7,4 +13,48 @@ package mm.amazon;
  * @author mmathuria
  */
 public class Q07 {
+    
+    public Map<String,Object> maxPath(BinaryTree btree){
+        return _maxPath(btree.getRoot());
+    }
+    
+    private Map<String,Object> _maxPath(TreeNode current){
+        if(current == null){
+            return null;
+        }
+        
+        
+        Map<String,Object> leftMap = _maxPath(current.leftChild);
+        Map<String, Object> rightMap =  _maxPath(current.rightChild);
+        
+        Map<String,Object> resultMap;
+        if(leftMap == null && rightMap == null){
+            Map<String,Object> map = new HashMap<String,Object>();
+            String nodeName = Integer.toString(current.getData());
+            map.put("path",nodeName);
+            map.put("sum",current.getData());
+            resultMap = map;
+        }else if(leftMap == null){
+            updateMap(current, rightMap);
+            resultMap = rightMap;
+        }else if(rightMap == null){
+            updateMap(current, leftMap);
+            resultMap = leftMap;
+        }else{
+            int leftSum = (Integer)leftMap.get("sum");
+            int rightSum = (Integer)rightMap.get("sum");
+            Map<String,Object> biggerMap = (leftSum > rightSum) ? leftMap : rightMap;
+            updateMap(current,biggerMap);
+            resultMap = biggerMap;
+        }
+        return resultMap;
+    }
+
+    private void updateMap(TreeNode current, Map<String, Object> map) {
+        String nodeName = Integer.toString(current.getData());
+        String updatedPath = nodeName + map.get("path");
+        Integer updateSum = current.getData() + ((Integer) map.get("sum"));
+        map.put("sum",updateSum);
+        map.put("path",updatedPath);
+    }
 }
