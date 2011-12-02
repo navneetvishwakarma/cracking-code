1133e2f7f9a082deaea29ee16f6b6157ab45d864
nphardcode@gmail.com <nphardcode@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2011-12-02 02:28:06 +0000

    more questions and simplified solution to question8

--- trunk/cracking/cracking/src/tarang/chap4/Question8.java
+++ trunk/cracking/cracking/src/tarang/chap4/Question8.java
@@ -14,66 +14,43 @@ import java.util.*;
  */
 public class Question8 {
 
-    private static void initializeMap(TreeNode node, Map<Integer, List<LinkedList<TreeNode>>> map) {
-        if(node == null) {
-            return;
-        }
-        List<LinkedList<TreeNode>> paths = map.get(node.getValue());
-        if(paths == null) {
-            paths = new ArrayList<LinkedList<TreeNode>>();
-            map.put(node.getValue(), paths);
-        }
-        initializeMap(node.getLeft(), map);
-        initializeMap(node.getRight(), map);
-    }
-
-    private static void traverseNodePath(TreeNode node, int sum, LinkedList<TreeNode> path,
-                                 Map<Integer, List<LinkedList<TreeNode>>> map) {
+    private static void traverseNodePath(TreeNode node, int X, int sum, LinkedList<TreeNode> path,
+                                 List<LinkedList<TreeNode>> paths) {
         if(node == null) {
             return;
         }
         int nodesum = sum + node.getValue();
-        if(map.get(nodesum) != null) {
+        if(nodesum == X) {
             path.add(node);
-            map.get(nodesum).add(path);
+            paths.add(path);
         }
-        traverseNodePath(node.getLeft(), nodesum, (LinkedList<TreeNode>) path.clone(), map);
-        traverseNodePath(node.getRight(), nodesum, (LinkedList<TreeNode>) path.clone(), map);
+        traverseNodePath(node.getLeft(), X, nodesum, (LinkedList<TreeNode>) path.clone(), paths);
+        traverseNodePath(node.getRight(), X, nodesum, (LinkedList<TreeNode>) path.clone(), paths);
     }
 
-    private static void traverse(TreeNode node, Map<Integer, List<LinkedList<TreeNode>>> map) {
+    private static void traverse(TreeNode node, int X, List<LinkedList<TreeNode>> paths) {
         if(node == null) {
             return;
         }
-        traverseNodePath(node, 0, new LinkedList<TreeNode>(), map);
-        traverse(node.getLeft(), map);
-        traverse(node.getRight(), map);
-    }
-
-
-    public static Map<Integer, List<LinkedList<TreeNode>>> nodePathSum(TreeNode root) {
-        Map<Integer, List<LinkedList<TreeNode>>> map  = new HashMap<Integer, List<LinkedList<TreeNode>>>();
-        // get a map with empty paths
-        initializeMap(root, map);
-        traverse(root, map);
-        return map;
+        traverseNodePath(node, X, 0, new LinkedList<TreeNode>(), paths);
+        traverse(node.getLeft(), X, paths);
+        traverse(node.getRight(), X, paths);
     }
 
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         TreeNode root = Question03.reconstruct_inorder(A, 0, A.length - 1);
-        Map<Integer, List<LinkedList<TreeNode>>> map = nodePathSum(root);
+        List<LinkedList<TreeNode>> paths = new ArrayList<LinkedList<TreeNode>>();
+        int X = 4;
+        traverse(root, X, paths);
         String str;
-        for(Integer key : map.keySet()) {
-            str = key + ":\n";
-            List<LinkedList<TreeNode>> list = map.get(key);
-            for(LinkedList<TreeNode> path : list) {
-                for(TreeNode node : path) {
-                    str += node.getValue() + " -> ";
-                }
-                str += "\n";
+        str = X + ":\n";
+        for (LinkedList<TreeNode> path : paths) {
+            for (TreeNode node : path) {
+                str += node.getValue() + " -> ";
             }
-            System.out.println(str);
+            str += "\n";
         }
+        System.out.println(str);
     }
 }
