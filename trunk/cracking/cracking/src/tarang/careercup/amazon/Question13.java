382bae9683d8d193a10f94073953751d79c5d4b2
nphardcode@gmail.com <nphardcode@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-04-04 18:05:57 +0000

    Answer to question13 and question55

---
+++ trunk/cracking/cracking/src/tarang/careercup/amazon/Question13.java
@@ -0,0 +1,47 @@
+package tarang.careercup.amazon;
+
+
+import tarang.datastructures.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * Given a binary tree and a number X, find if there exists a path that sums to X. Note that the path may or may not
+ * start from the root
+ *
+ * @author tdesai
+ */
+public class Question13 {
+
+    public static void findPathForX(TreeNode node, int X, int level, ArrayList<Integer> path) {
+        if(node == null) {
+            return;
+        }
+        path.add(node.getValue());
+        int sum = X;
+        for(int i = level; i >= 0; i--) {
+            sum -= path.get(i);
+            if(sum == 0) {
+                // print path from i to level.
+                for(int j = i; j <= level; j++) {
+                    System.out.print(path.get(j));
+                    if(j < level) {
+                        System.out.print(" -> ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        ArrayList<Integer> c1 = (ArrayList<Integer>) path.clone();
+        ArrayList<Integer> c2 = (ArrayList<Integer>) path.clone();
+        findPathForX(node.getLeft(), X, level+1, c1);
+        findPathForX(node.getRight(), X, level+1, c2);
+    }
+
+    public static void main(String[] args) {
+        int[] A = {3, 6, 0, 2, 3, 1, 4, 0, 4, 5, 9, 0, 1, 11, 10};
+        TreeNode root = Question03.reconstruct_preorder(A, 0, A.length - 1);
+
+        findPathForX(root, 7, 0, new ArrayList<Integer>());
+    }
+}
