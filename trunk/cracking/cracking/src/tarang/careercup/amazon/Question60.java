98c93eb0a05f337c76af574114ed098d18f69463
nphardcode@gmail.com <nphardcode@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-04-11 17:43:40 +0000

    Answer to question60. Initial commit.

---
+++ trunk/cracking/cracking/src/tarang/careercup/amazon/Question60.java
@@ -0,0 +1,24 @@
+package tarang.careercup.amazon;
+
+
+import tarang.datastructures.TreeNode;
+
+/**
+ * Return true if two trees are same
+ *
+ * @author tdesai
+ */
+public class Question60 {
+
+    public static boolean same(TreeNode node1, TreeNode node2) {
+        if(node1 == null && node2 == null) {
+            return true;
+        }
+        if(node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.getValue() == node2.getValue()
+                && same(node1.getLeft(), node2.getLeft())
+                && same(node1.getRight(), node2.getRight());
+    }
+}
