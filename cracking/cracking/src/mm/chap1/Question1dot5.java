57f8b09342a89ddad1b6c5c9b0fd083f198ee95a
aatmaram.dubey <aatmaram.dubey@gmail.com>
2012-04-24 21:57:48 +0000

    1.5/1.8

--- cracking/cracking/src/mm/chap1/Question1dot5.java
+++ cracking/cracking/src/mm/chap1/Question1dot5.java
@@ -32,11 +32,51 @@ public class Question1dot5 {
         return new String(c);
     }
 
+    //same as replacedSpaces() but the while loop moves from 0 to end (instead of end to 0 above)
+    public static String replacedSpaces2(String str){
+        int spaceCount = 0;
+        char[] s = str.toCharArray();
+        for(char c : s){
+            if(c == ' ') spaceCount++;
+        }
+        
+        char[] c = new char[str.length() + (spaceCount*2)];
+        
+        int i = 0;
+        int j = 0;
+        while(i < s.length){
+            if(s[i] == ' '){
+                c[j] = '%';
+                c[++j] = '2';
+                c[++j] = '0';
+            }else{
+                c[j] = s[i];
+            }
+            i++;j++;
+        }
+        return new String(c);
+    }
+    
+    //with stringbuilder
+    public static String replacedSpaces3(String str){
+        StringBuilder buf = new StringBuilder();
+        for(char c : str.toCharArray()){
+            if(c == ' '){
+                buf.append('%');
+                buf.append('2');
+                buf.append('0');
+            }else{
+                buf.append(c);
+            }
+        }
+        return buf.toString();
+    }
+
     public static void main(String[] args) {
         String str1 = "new apple device rocks";
-        String str2 = "rocks";
-        System.out.println(replacedSpaces(str1));        
-        System.out.println(replacedSpaces(str2));        
-        System.out.println(replacedSpaces(" "));        
+        String str2 = " rocks ";
+        System.out.println(replacedSpaces3(str1));        
+        System.out.println(replacedSpaces3(str2));        
+        System.out.println(replacedSpaces3(" "));
     }
 }
---
+++ cracking/cracking/src/mm/chap1/Question1dot8.java
@@ -0,0 +1,25 @@
+package mm.chap1;
+
+
+/**
+ * Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, 
+ * write code to check if s2 is a rotation of s1 using only one call to isSubstring 
+ * (i e , “waterbottle” is a rotation of “erbottlewat”)
+ *
+ * @author mmathuria
+ */
+public class Question1dot8 {
+    
+    public static boolean isRotation(String s1, String s2){
+        return isSubstring(s1 + s1, s2);
+    }
+    
+    private static boolean isSubstring(String s1, String s2){
+        return s1.contains(s2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isRotation("hello","lohel"));
+        System.out.println(isRotation("hello","lohee"));
+    }
+}
