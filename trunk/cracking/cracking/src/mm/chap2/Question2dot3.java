d4274a0a8b275dd2f461db5ca74603afac945b4d
aatmaram.dubey@gmail.com <aatmaram.dubey@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-04-25 18:44:49 +0000

    q2.3

---
+++ trunk/cracking/cracking/src/mm/chap2/Question2dot3.java
@@ -0,0 +1,24 @@
+package mm.chap2;
+
+
+import mm.Node;
+
+/**
+ Implement an algorithm to delete a node in the middle of a single linked list, given only access to that node
+   EXAMPLE
+   Input: the node ‘c’ from the linked list a->b->c->d->e
+   Result: nothing is returned, but the new linked list looks like a->b->d->e
+ * @author mmathuria
+ */
+public class Question2dot3 {
+    
+    public boolean deleteNode(Node n){
+        if(n.next == null){
+            return false;   //cannot delete the last node
+        } else {
+            n.data = n.next.data;
+            n.next = n.next.next;
+        }
+        return true;
+    }
+}
---
+++ trunk/cracking/cracking/test/mm/chap2/Question2dot3Test.java
@@ -0,0 +1,37 @@
+package mm.chap2;
+
+
+import junit.framework.TestCase;
+import mm.LinkedList;
+import mm.Node;
+
+import java.util.LinkedHashSet;
+
+/**
+ * Put class JavaDoc here
+ *
+ * @author mmathuria
+ */
+public class Question2dot3Test extends TestCase {
+    Question2dot3 testObj = new Question2dot3();
+    
+    public void testDeleteNonLastNode(){
+        LinkedList list = new LinkedList();
+        Node nodeToBeDeleted = new Node('C');
+        list.append(new Node('A')).append(new Node('B')).append(nodeToBeDeleted).append(new Node('D'));
+        
+        int originalListSize = list.count();
+        assertTrue(testObj.deleteNode(nodeToBeDeleted));
+        assertEquals("list size does not match",(originalListSize-1),list.count());
+    }
+    
+    public void testDeleteLastNode(){
+        LinkedList list = new LinkedList();
+        Node nodeToBeDeleted = new Node('D');
+        list.append(new Node('A')).append(new Node('B')).append(new Node('C')).append(nodeToBeDeleted);
+
+        int originalListSize = list.count();
+        assertFalse("did not return false",testObj.deleteNode(nodeToBeDeleted));
+        assertEquals("list size does not match",originalListSize,list.count());
+    }
+}
