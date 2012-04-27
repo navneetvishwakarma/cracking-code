739c39ff72f911531fe4b5bd9537a9548e9ab315
nphardcode <nphardcode@gmail.com>
2012-04-27 21:52:02 +0000

    CLEANING UP

--- cracking/cracking/src/tarang/topcoder/Prerequisites.java
+++ cracking/cracking/src/tarang/topcoder/Prerequisites.java
@@ -12,12 +12,12 @@ public class Prerequisites {
 
     private void sort(ClassNode node, List<String> result) {
         for(ClassNode prereq : node.getPrereqs()) {
-            if (!prereq.isVisited()) {
+            if (!prereq.visited) {
                 sort(prereq, result);
             }
         }
         result.add(node.toString());
-        node.setVisited(true);
+        node.visited = true;
     }
 
     public String[] orderClasses(String[] strs) {
@@ -33,7 +33,7 @@ public class Prerequisites {
                 orig = temp;
                 classes.put(orig.toString(), orig);
             } else {
-                orig.setEntry(true);
+                orig.entry = true;
             }
 
             for(int i = 1; i < tokens.length; i++) {
@@ -49,49 +49,27 @@ public class Prerequisites {
             }
         }
         for(ClassNode node : classes.values()) {
-            if(!node.isEntry()) {
+            if(!node.entry) {
                 return new String[]{};
             }
         }
         // create the list of classes
         List<String> result = new LinkedList<String>();
         for(ClassNode node : new TreeSet<ClassNode>(classes.values())) {
-            if (!node.isVisited()) {
+            if (!node.visited) {
                 sort(node, result);
             }
         }
         return result.toArray(new String[result.size()]);
     }
-
-/*    public static void main(String[] args) {
-        String[] classes = new String[]{
-                "CSE258: CSE244 CSE243 INTR100",
-                "CSE221: CSE254 INTR100",
-                "CSE254: CSE111 MATH210 INTR100",
-                "CSE244: CSE243 MATH210 INTR100",
-                "MATH210: INTR100",
-                "CSE101: INTR100",
-                "CSE111: INTR100",
-                "ECE201: CSE111 INTR100",
-                "ECE111: INTR100",
-                "CSE243: CSE254",
-                "INTR100:"
-        };
-
-        *//*classes = new String[]{
-                "ENGL111: ENGL110"
-        };*//*
-
-        new Prerequisites().orderClasses(classes);
-    }*/
 }
 
 class ClassNode implements Comparable {
-    private String dept;
-    private Integer classNum;
-    private Set<ClassNode> prereqs;
-    private boolean visited;
-    private boolean entry;
+    String dept;
+    Integer classNum;
+    Set<ClassNode> prereqs;
+    boolean visited;
+    boolean entry;
 
     public ClassNode(String classDeptNum, boolean entry) {
         if(classDeptNum.endsWith(":")) {
@@ -99,17 +77,8 @@ class ClassNode implements Comparable {
         }
         this.classNum = Integer.valueOf(classDeptNum.substring(classDeptNum.length() - 3, classDeptNum.length()));
         this.dept = classDeptNum.substring(0, classDeptNum.indexOf(this.classNum + ""));
-
         this.visited = false;
         this.entry = entry;
-    }
-
-    public String getDept() {
-        return dept;
-    }
-
-    public Integer getClassNum() {
-        return classNum;
     }
 
     public Set<ClassNode> getPrereqs() {
@@ -117,22 +86,6 @@ class ClassNode implements Comparable {
             this.prereqs = new TreeSet<ClassNode>();
         }
         return prereqs;
-    }
-
-    public boolean isVisited() {
-        return visited;
-    }
-
-    public void setVisited(boolean visited) {
-        this.visited = visited;
-    }
-
-    public boolean isEntry() {
-        return entry;
-    }
-
-    public void setEntry(boolean entry) {
-        this.entry = entry;
     }
 
     public boolean addPrereq(ClassNode node) {
@@ -145,15 +98,9 @@ class ClassNode implements Comparable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
         ClassNode classNode = (ClassNode) o;
-
         if (!classNum.equals(classNode.classNum)) {
             return false;
         }
@@ -164,7 +111,6 @@ class ClassNode implements Comparable {
         return true;
     }
 
-    @Override
     public int hashCode() {
         int result = dept.hashCode();
         result = 31 * result + classNum.hashCode();
@@ -172,26 +118,18 @@ class ClassNode implements Comparable {
     }
 
     public int compareTo(Object o) {
-        if (this == o) {
-            return 0;
-        }
-
+        if (this == o) { return 0; }
         ClassNode that = (ClassNode) o;
-
-        if(this.classNum.equals(that.getClassNum()) && this.dept.equals(that.getDept())) {
-            return 0;
-        }
-
-        if(this.classNum < that.getClassNum()) {
+        if(this.classNum.equals(that.classNum) && this.dept.equals(that.dept)) { return 0; }
+        if(this.classNum < that.classNum) {
             return -1;
-        } else if(this.classNum > that.getClassNum()) {
+        } else if(this.classNum > that.classNum) {
             return 1;
         } else {
-            return this.getDept().compareTo(that.getDept());
+            return this.dept.compareTo(that.dept);
         }
     }
 
-    @Override
     public String toString() {
         return dept + classNum;
     }
