55c3dec9ba52534d6788c5b687d69e42417df108
nphardcode <nphardcode@gmail.com>
2012-05-05 22:36:29 +0000

    clout solution

--- cracking/cracking/src/tarang/chap4/Question5.java
+++ cracking/cracking/src/tarang/chap4/Question5.java
@@ -4,7 +4,7 @@ import tarang.datastructures.TreeNode;
 
 
 /**
- * Write an algorithm to find the ‘next’ node (i e , in-order successor) of a given node in a binary search tree where
+ * Write an algorithm to find the next node (i e , in-order successor) of a given node in a binary search tree where
  * each node has a link to its parent
  *
  * @author tdesai
