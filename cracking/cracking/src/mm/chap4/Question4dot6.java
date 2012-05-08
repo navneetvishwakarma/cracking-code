b2b6c50039114e91aafe15bee89a128f9b310c02
nphardcode <nphardcode@gmail.com>
2012-05-08 00:12:22 +0000

    test cases

--- cracking/cracking/src/mm/chap4/Question4dot6.java
+++ cracking/cracking/src/mm/chap4/Question4dot6.java
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2001-2008 Bridge Entertainment, Inc. (dba E-Poll Market Research)
  * All Rights Reserved.
- * 
+ *
  * This software is the confidential and proprietary information
  * of Bridge Entertainment, Inc. ("Confidential Information").
  */
@@ -16,14 +16,14 @@ import mm.TreeNode;
 
 
 /**
- * Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree Avoid storing 
+ * Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree Avoid storing
  * additional nodes in a data structure	NOTE: This is not necessarily a binary search tree
  *
  * @author mmathuria
  */
 public class Question4dot6 {
-    
-    public TreeNode findCommonAncestor(BinaryTree tree, TreeNode node1, TreeNode node2){
+
+    public static TreeNode findCommonAncestor(BinaryTree tree, TreeNode node1, TreeNode node2){
         if(tree == null) throw new IllegalStateException("Tree cannot be empty");
         TreeNode ancestor = find(tree.getRoot(),node1, node2);
         if(ancestor!=null && (ancestor.getData() == node1.getData()
@@ -32,19 +32,19 @@ public class Question4dot6 {
         }
         return ancestor;
     }
-    
-    private TreeNode find(TreeNode current, TreeNode node1, TreeNode node2){
+
+    private static TreeNode find(TreeNode current, TreeNode node1, TreeNode node2){
         if(current == null) return null;
-        System.out.print(current.getData() + ", ");
+        //System.out.print(current.getData() + ", ");
 
         if(current.getData() == node1.getData()
                 || current.getData() == node2.getData()){
             return current;
         }
-        
+
         TreeNode left = find(current.getLeftChild(), node1, node2);
         TreeNode right = find(current.getRightChild(), node1, node2);
-        
+
         if(left!= null && right != null){
             return current;
         }else if(left == null){
@@ -53,5 +53,35 @@ public class Question4dot6 {
             return left;
         }
     }
-    
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree(8);
+
+        TreeNode seven = new TreeNode(7);
+        TreeNode five = new TreeNode(3);
+        TreeNode nine = new TreeNode(9);
+        TreeNode four = new TreeNode(4);
+        TreeNode one = new TreeNode(1);
+        TreeNode six = new TreeNode(6);
+        TreeNode three = new TreeNode(3);
+
+        four.leftChild = one;
+        four.rightChild = six;
+
+        nine.leftChild = four;
+        nine.rightChild = three;
+
+        five.leftChild = nine;
+
+        tree.getRoot().leftChild = seven;
+        tree.getRoot().rightChild = five;
+
+        System.out.println(findCommonAncestor(tree, seven, six).data);  // 8
+        System.out.println(findCommonAncestor(tree, one, six).data);    // 4
+        System.out.println(findCommonAncestor(tree, one, three));   // null
+        System.out.println(findCommonAncestor(tree, four, three));  // null
+        System.out.println(findCommonAncestor(tree, seven, five));  // 8
+        System.out.println(findCommonAncestor(tree, seven, three)); // 8
+    }
+
 }
