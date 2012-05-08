c654158def5bdc0bb57d61ed38a47030c5823bdb
nphardcode <nphardcode@gmail.com>
2012-05-08 00:15:17 +0000

    compile errr

--- cracking/cracking/src/tarang/designpatterns/command/LightOnCommand.java
+++ cracking/cracking/src/tarang/designpatterns/command/LightOnCommand.java
@@ -6,19 +6,17 @@ package tarang.designpatterns.command;
  *
  * @author tdesai
  */
-public class LightOnCommand extends Command {
+public class LightOnCommand implements Command {
     private Light light;
 
     public LightOnCommand(Light light) {
         this.light = light;
     }
 
-    @Override
     public void execute() {
         light.on();
     }
 
-    @Override
     public void undo() {
         light.off();
     }
