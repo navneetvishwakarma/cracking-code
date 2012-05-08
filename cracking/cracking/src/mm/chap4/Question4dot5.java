c983e979e3983cb3650abc594014a9a3cf5948af
aatmaram.dubey <aatmaram.dubey@gmail.com>
2012-05-08 00:27:59 +0000

    typo

--- cracking/cracking/src/mm/chap4/Question4dot5.java
+++ cracking/cracking/src/mm/chap4/Question4dot5.java
@@ -18,9 +18,9 @@ public class Question4dot5 {
             return leftMostNode(node.rightChild);
         }else{
             if(node.parent == null) return null;
-            else if(node.parent.leftChild.data == node.data) return node.parent;
+            else if(node.parent.leftChild.data == node.data) return node.parent;    //the node is the leftchild of a non-visited parent. return the parent
             else{
-                TreeNode current = node;
+                TreeNode current = node;    //keep traversing up till we find a non-visited parent
                 while(current != null && current.data <= node.data) current = current.parent;
                 return current;
             }
