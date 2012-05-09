74f2ea3179e37e5ff4e775d8e6197b5cf83eb193
aatmaram.dubey <aatmaram.dubey@gmail.com>
2012-05-09 18:14:57 +0000

    qn4.8

--- cracking/cracking/src/mm/chap4/Question4dot7.java
+++ cracking/cracking/src/mm/chap4/Question4dot7.java
@@ -1,12 +1,12 @@
 package mm.chap4;
 
 
-import mm.BinaryTree;
 import mm.TreeNode;
 
 /**
- * Put class JavaDoc here
- *
+ * You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes	Create an
+ * algorithm to decide if T2 is a subtree of T1
+ * 
  * @author mmathuria
  */
 public class Question4dot7 {
