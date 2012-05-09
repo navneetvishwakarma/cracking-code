e48d2197b697e91170d1cc149780b4d69166f7bf
aatmaram.dubey@gmail.com <aatmaram.dubey@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-05-09 18:14:57 +0000

    qn4.8

---
+++ trunk/cracking/cracking/src/mm/chap4/Question4dot8.java
@@ -0,0 +1,57 @@
+package mm.chap4;
+
+
+import mm.BinaryTree;
+import mm.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * Q13.java also solves this problem with a different (inefficient and complicated) approach
+ * 
+ * You are given a binary tree in which each node contains a value Design an algorithm to print all paths which sum up 
+ * to that value Note that it can be any path in the tree - it does not have to start at the root
+ *
+ * @author mmathuria
+ */
+public class Question4dot8 {
+    public void findPath(TreeNode node, int x, ArrayList<Integer> list){
+        if(node == null){
+            return;
+        }
+        
+        int sum = 0;
+        list.add(node.data);
+        for(int i=list.size()-1;i>=0;i--){
+            sum += list.get(i);
+            if(sum == x){
+                print(list,i);
+            }
+        }
+
+        //noinspection unchecked
+        findPath(node.leftChild,x,(ArrayList<Integer>) list.clone());
+        //noinspection unchecked
+        findPath(node.rightChild,x, (ArrayList<Integer>) list.clone());
+    }
+    
+    
+    private void print(ArrayList<Integer> list, int i){
+        StringBuilder buf = new StringBuilder();
+        for(int i2 = list.size()-1; ; i2--){
+            buf.append(list.get(i2));
+            if(i2 <= i){
+                break;
+            }
+            buf.append("-->");
+        }
+        System.out.println(buf);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2,8,24,12,6,8,10,-12,16,18,14};
+        BinaryTree tree = new Question4dot3().createMinimalBT(a);
+
+        new Question4dot8().findPath(tree.getRoot(),100,new ArrayList<Integer>());
+    }
+}
