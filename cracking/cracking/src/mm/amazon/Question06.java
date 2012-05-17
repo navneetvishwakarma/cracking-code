49ab784e2414058ce1fb2a789798cb47c7d5db75
aatmaram.dubey <aatmaram.dubey@gmail.com>
2012-05-17 18:44:26 +0000

    qn06

---
+++ cracking/cracking/src/mm/amazon/Question06.java
@@ -0,0 +1,34 @@
+package mm.amazon;
+
+
+import mm.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * Given a binary tree count the number of nodes at each level in the tree. Follow up: count the number of nodes below 
+ * each node
+ */
+public class Question06 {
+    
+    public void countNodesAtEachLevel(TreeNode node, int depth, ArrayList<Integer> list){
+        if(node == null) return;
+
+        try {
+            Integer currentCount = list.get(depth);
+            list.add(depth,currentCount+1);
+        } catch (Exception e) {
+            list.add(depth,1);
+        }
+ 
+        countNodesAtEachLevel(node.leftChild,depth+1,list);
+        countNodesAtEachLevel(node.rightChild,depth+1,list);
+    }
+    
+    public int countNodesBelowGiven(TreeNode node){
+        if(node == null) return 0;
+        
+        return 1 + countNodesBelowGiven(node.leftChild) + countNodesBelowGiven(node.rightChild);
+    }
+    
+}
---
+++ cracking/cracking/test/mm/amazon/Question06Test.java
@@ -0,0 +1,51 @@
+package mm.amazon;
+
+
+import junit.framework.TestCase;
+import mm.BinaryTree;
+
+import java.util.ArrayList;
+
+/**
+ * Put class JavaDoc here
+ */
+public class Question06Test extends TestCase {
+    Question06 testObj = new Question06();
+    
+    public void testCountNodesAtEachLevel(){
+
+        BinaryTree tree = Q03_BT.createFromInOrder(new int[]{3,4,5,6,7,9,17,20,22});
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        testObj.countNodesAtEachLevel(tree.getRoot(),0,output);
+        
+        assertNotNull("output is null",output);
+        assertEquals("", new Integer(1), output.get(0));
+        assertEquals("", new Integer(2), output.get(1));
+        assertEquals("",new Integer(3),output.get(2));
+        assertEquals("",new Integer(3),output.get(3));
+
+        System.out.println(new ArrayList<Integer>().size());
+    }
+
+    public void testCountNodesAtEachLevelEmptyTree(){
+        
+        BinaryTree tree = new BinaryTree();
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        testObj.countNodesAtEachLevel(tree.getRoot(),0,output);
+        
+        assertNotNull("output is null", output);
+        assertTrue("output is not empty",output.isEmpty());
+    }
+    
+    public void testCountNodesBelow(){
+
+        BinaryTree tree = Q03_BT.createFromInOrder(new int[]{3,4,5,6,7,9,17,20,22});
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        int count = testObj.countNodesBelowGiven(tree.getRoot().leftChild);
+
+        assertNotNull("output is null", output);
+        assertEquals("count does not match",3,count-1);
+    }
+    
+    
+}
