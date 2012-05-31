e2014dfa5b762f8a26beb74594dd7827df916571
aatmaram.dubey@gmail.com <aatmaram.dubey@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-05-31 21:25:35 +0000

    q32

---
+++ trunk/cracking/cracking/src/mm/amazon/Q32.java
@@ -0,0 +1,59 @@
+package mm.amazon;
+
+
+/**
+ * Given an array having positive integers, find a continous subarray which adds to a given number
+ */
+public class Q32 {
+    
+    public static void continuousSubArray(int[] data, int num){
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        
+        while (j < data.length){
+            sum += data[j++];
+            
+            if(sum > num){
+                while (sum > num){
+                    sum -= data[i++];
+                }
+            }
+            
+            if(sum == num){
+                break;
+            }
+        }
+        
+        if(sum != num){
+            System.out.println("no subarray found = " + num);
+            return;
+        }
+        printResult(i, j-1, data, num);
+    }
+    
+
+    public static void main(String[] args) {
+        continuousSubArray(new int[]{2,4,1,3,7,5,6},11);
+        continuousSubArray(new int[]{2,4,1,3,7,5,6},16);
+        continuousSubArray(new int[]{2,4,1,3,7,5,6},13);
+        continuousSubArray(new int[]{2,4,1,3,7,5,6},28);
+        continuousSubArray(new int[]{1},2);
+        continuousSubArray(new int[]{2},2);
+        continuousSubArray(new int[]{3},2);
+    }
+
+
+    private static void printResult(int i, int j, int[] data, int num){
+        StringBuilder buf = new StringBuilder();
+        while(i <= j){
+            buf.append(data[i]);
+            if(i < j){
+                buf.append(" + ");
+            }
+            i++;
+        }
+        buf.append(" = ").append(num);
+        System.out.println(buf.toString());
+    }
+}
