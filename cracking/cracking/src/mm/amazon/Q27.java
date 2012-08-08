671678728ec2e3aab70618cf0ed1012fe192c558
aatmaram.dubey <aatmaram.dubey@gmail.com>
2012-08-08 01:18:54 +0000

    refactoring

--- cracking/cracking/src/mm/amazon/Q27.java
+++ cracking/cracking/src/mm/amazon/Q27.java
@@ -7,23 +7,22 @@ package mm.amazon;
 public class Q27 {
     
     //the essence is to have a loop that keeps halving the exp and squaring the base. and if for a given iteration
-    //the exp is an odd number, remainingMult var is updated
+    //the exp is an odd number the variable 'remainder' is updated
     public static long power(int base, int exp){
-        long remainingMult = 1;
-        long result = base;
+        long remainder = 1;
         
         while (exp > 1){
-            int remainder = exp % 2;
-            if(remainder > 0){
-                remainingMult = remainingMult * result;
+            if(exp % 2 ==  1){
+                remainder = remainder * base;
+                exp--;
             }
-            exp = (exp - remainder) / 2;
-            result *= result;
+            exp = exp / 2;
+            base *= base;
         }
-        return result * remainingMult;
+        return base * remainder;
     }
 
     public static void main(String[] args) {
-        System.out.println(power(2,10));
+        System.out.println(power(2,11));
     }
 }
