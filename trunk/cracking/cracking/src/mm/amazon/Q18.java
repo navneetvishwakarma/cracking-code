fece996fe960f353073e5f940603ad67885b6f68
aatmaram.dubey@gmail.com <aatmaram.dubey@gmail.com@8840930a-9cda-c60b-bd53-169ac780652f>
2012-08-08 01:17:01 +0000

    better naming

--- trunk/cracking/cracking/src/mm/amazon/Q18.java
+++ trunk/cracking/cracking/src/mm/amazon/Q18.java
@@ -3,21 +3,23 @@ package mm.amazon;
 
 /**
  * Given an unsorted Array find maximum value of A[j] - A[i] where j>i..in O(n) time
+ * OR
+ * Given an array of stock prices for each day's closing price find the highest Profit possible in O(n)
  */
 public class Q18 {
-    public static void findMaxDiff(int[] data){
-        int min = data[0];
+    public static void highestProfit(int[] prices){
+        int min = prices[0];
         int diff = 0;
         int num1=-1;
         int num2=-1;
 
-        for(int i=1; i<data.length;i++){
-            if(data[i] < min){  //if the number is min, no point in comparing it with anything on the left. 
-                min = data[i];  //assign to min and continue
-            }else if ( (data[i] - min) > diff){
-                diff = (data[i] - min); //if the diff of this num and the min is greater than existing diff
+        for(int i=1; i<prices.length;i++){
+            if(prices[i] < min){  //if the number is min, no point in comparing it with anything on the left. 
+                min = prices[i];  //assign to min and continue
+            }else if ( (prices[i] - min) > diff){
+                diff = (prices[i] - min); //if the diff of this num and the min is greater than existing diff
                 num1 = min;             //we have found a new max and greater a diff
-                num2 = data[i];
+                num2 = prices[i];
             }
         }
         System.out.println(num2 + "-" + num1 + "=" + diff);
@@ -36,8 +38,8 @@ public class Q18 {
     }
 
     public static void main(String[] args){
-        Q18.findMaxDiff(new int[]{8,7,18,2,3,1,0,4,5});
-        Q18.findMaxDiff(new int[]{8,7,6,2,3,1,0,4,5});
-        Q18.findMaxDiff(new int[]{10,5,14,8,16,4,1,7,18,2});
+        Q18.highestProfit(new int[]{8, 7, 18, 2, 3, 1, 0, 4, 5});
+        Q18.highestProfit(new int[]{8, 7, 6, 2, 1, 3, 8, 2, 5});
+        Q18.highestProfit(new int[]{22, 8, 16, 4, 2, 7, 18, 6, 1});
     }
 }
